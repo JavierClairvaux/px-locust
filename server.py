ba5deb47dd63942d3bdc630926c8709fefdf2bca
JavierClairvaux <javcabr@gmail.com>
2020-06-16 16:09:18 -0700

    changed j for res

--- server.py
+++ server.py
@@ -10,46 +10,46 @@ db.set('running', False)
 @app.route('/invokust', methods = ['POST'])
 def postJsonHandler():
     if db.get('running'):
-        j = {
+        res = {
             'pid': db.get('pid'),
             'running': db.get('running')
                 }
-        return jsonify(j)
-    content = request.get_json()
+        return ressonify(res)
+    content = request.get_resson()
     proc = subprocess.Popen(["locust", "-r", content["hrate"], "-u", content["users"], "-f", "/locust/locustfile.py", "-H", os.environ['FRONTEND_ADDR'], "--headless"] )
     db.set('running', True)
     db.set('pid', proc.pid)
-    j = {
+    res = {
         'pid': db.get('pid'),
         'running': db.get('running')
             }
-    return jsonify(j)
+    return ressonify(res)
 
 @app.route('/invokust', methods = ['DELETE'])
 def stopLocust():
     if not db.get('running'):
-        j = {
+        res = {
             'pid': db.get('pid'),
             'running': db.get('running')
                 }
-        return jsonify(j)
+        return ressonify(res)
 
     os.kill(db.get('pid'), signal.SIGTERM)
     db.set('running', False)
     db.set('pid', 0)
-    j = {
+    res = {
         'pid': db.get('pid'),
         'running': db.get('running')
             }
-    return jsonify(j)
+    return ressonify(res)
 
 @app.route('/invokust', methods = ['GET'])
 def getLocust():
-    j = {
+    res = {
         'pid': db.get('pid'),
         'running': db.get('running')
             }
-    return jsonify(j)
+    return ressonify(res)
 
 app.run(host='0.0.0.0')
 
